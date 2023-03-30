4fb9516c4fc06342daa7a32845abd4ed8d65610f
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2023-03-30 21:03:12 +0300

    Task1_1

---
+++ task1_1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;;
+
+public class task1_1 {
+    public static void main(String[] args) {
+        System.out.println("\n\nType num: ");
+
+        Scanner sc = new Scanner(System.in);  
+        int num = sc.nextInt();
+        int sum = 0;
+        int factorial = 1;
+
+        for (int i = 1; i <= num; i++) {
+            sum = sum + i;
+            factorial = factorial * i;
+        }
+
+        System.out.println("Sum is " + sum);
+        System.out.println("Factoorial is " + factorial + "\n\n");
+    }
+    
+}
